f8db03647bbe1a86b232d192897d75fad2c945a1
Shaun Lobsinger <shaun.lobsinger@gmail.com>
2024-04-24 13:57:00 -0700

    fixed my headers config

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,21 @@ const nextConfig = {
     async headers() {
         return [
             {
-                key: "Access-Control-Allow-Origin",
-                value: "*"
-            },
-            {
-                key: "Access-Control-Allow-Methods",
-                value: "GET"
-            },
-            {
-                key: "Access-Control-Allow-Headers",
-                value: "Content-Type, Authorization",
+                source: "/api/(.*)",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*"
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET"
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
+                    }
+                ]
             }
         ]
     }
