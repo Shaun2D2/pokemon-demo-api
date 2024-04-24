9d4bdc777a6a7992d9ae1310b460ffa6f8ed487f
Shaun Lobsinger <shaun.lobsinger@gmail.com>
2024-04-24 13:55:10 -0700

    updated config for CORS calls

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                key: "Access-Control-Allow-Origin",
+                value: "*"
+            },
+            {
+                key: "Access-Control-Allow-Methods",
+                value: "GET"
+            },
+            {
+                key: "Access-Control-Allow-Headers",
+                value: "Content-Type, Authorization",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
